dc5ad96fe06c3115224fbf9b6ae85418976912c7
krios2146 <krios2146@gmail.com>
2025-12-14 15:31:44 +0700

    chore: remove logs for release build

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,6 @@ use pelite::{
 };
 use std::{
     collections::HashMap,
-    fs::OpenOptions,
-    io::Write,
     ptr::read_unaligned,
     time::Duration,
 };
@@ -28,18 +26,6 @@ const PARAM_BASE_PATTERN_STR: &str = "48 8B 0D ? ? ? ? ? ? ? ? ? ? ? E8 ? ? ? ? 
 
 const OFFSET: usize = 3;
 const ADDITIONAL: usize = 7;
-
-fn log(s: &str) {
-    let now = chrono::Local::now()
-        .format("%y.%m.%d | %H:%M:%S.%3f")
-        .to_string();
-    let _ = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(r"C:\Program Files (x86)\Steam\steamapps\common\ELDEN RING\Game\mod_dll\starlight-shards-as-rune-arcs\logs.log")
-        .map(|mut f| writeln!(f, "[{}]: {}", now, s))
-        .unwrap();
-}
 
 fn get_pe_view() -> PeView<'static> {
     let pe_view = match Program::current() {
@@ -56,14 +42,8 @@ fn get_executable_header(pe: PeView<'_>) -> &SectionHeader {
         .iter()
         .find(|h| &h.Characteristics & IMAGE_SCN_MEM_EXECUTE != 0)
     {
-        Some(h) => {
-            log("Executable header found");
-            h
-        }
-        None => {
-            log("Executable header not found");
-            panic!()
-        }
+        Some(h) => h,
+        None => panic!()
     };
     executable_header
 }
@@ -71,8 +51,7 @@ fn get_executable_header(pe: PeView<'_>) -> &SectionHeader {
 fn find_param_base_rva(pe_view: PeView) -> usize {
     let pattern = match pattern::parse(PARAM_BASE_PATTERN_STR) {
         Ok(p) => p,
-        Err(e) => {
-            log(&format!("pattern parse error: {}", e.to_string()));
+        Err(_) => {
             panic!()
         }
     };
@@ -84,12 +63,10 @@ fn find_param_base_rva(pe_view: PeView) -> usize {
     let mut matches = scanner.matches(&*pattern, executable_header.file_range());
 
     if !matches.next(&mut matched_rva) {
-        log(&format!("No RVA found for pattern {:?}", PARAM_BASE_PATTERN_STR));
         panic!()
     }
 
     let base_param_ptr_rva = matched_rva[0] as usize;
-    log(&format!("Base param RVA {:?}", base_param_ptr_rva));
 
     base_param_ptr_rva
 }
@@ -97,15 +74,12 @@ fn find_param_base_rva(pe_view: PeView) -> usize {
 fn get_param_base_ptr(base: *const u8, base_param_rva: usize) -> *const u64 {
     unsafe {
         let base_param_ptr_va: *const u8 = base.add(base_param_rva);
-        log(&format!("Base param pointer without offset VA [{:?}]", base_param_ptr_va));
 
         // readInteger(foundaddr+3,true) where true = signed
         let offset_value: i32 = read_unaligned(base_param_ptr_va.add(OFFSET) as *const i32);
-        log(&format!("Offset value for base param pointer [{:?}]", offset_value));
 
         // foundaddr+7+readInteger(foundaddr+3,true)
         let base_param_ptr_va: *const u64 = base_param_ptr_va.add(ADDITIONAL).offset(offset_value as isize) as *const u64;
-        log(&format!("Base param pointer with offset VA [{:?}]", base_param_ptr_va));
 
         base_param_ptr_va
     }
@@ -115,19 +89,15 @@ fn get_param_speffect_ptr(param_base_ptr: *const u64) -> *const u64 {
     unsafe {
         // readQword(GetParamBasePtr()) where GetParamBasePtr() is foundaddr+7+readInteger(foundaddr+3,true)
         let base_param_va: *const u64 = read_unaligned(param_base_ptr) as *const u64;
-        log(&format!("Base param VA [{:?}]", base_param_va));
 
         // local hdr=readQword(ParamBase+Index*72+0x88)
         let hdr: *const u64 = read_unaligned((base_param_va as *const u8).add(SPEFFECTS_INDEX * 72 + 0x88) as *const u64) as *const u64;
-        log(&format!("Header [{:?}]", hdr));
 
         // readQword(hdr+0x80)
         let param_goods_ptr: *const u64 = read_unaligned((hdr as *const u8).add(0x80) as *const u64) as *const u64;
-        log(&format!("readQword(hdr+0x80) [{:?}]", param_goods_ptr));
 
         // readQword(readQword(hdr+0x80)+0x80)
         let param_goods_ptr: *const u64 = read_unaligned((param_goods_ptr as *const u8).add(0x80) as *const u64) as *const u64;
-        log(&format!("readQword(readQword(hdr+0x80)+0x80) [{:?}]", param_goods_ptr));
 
         param_goods_ptr
     }
@@ -137,7 +107,7 @@ fn get_speffect_list_size(param_goods_ptr: *const u64) -> u16 {
     unsafe {
         // local n = readSmallInteger(TableBase + 10)
         let table_size: u16 = read_unaligned((param_goods_ptr as *const u8).add(10) as *const u16);
-        log(&format!("Table size: {}", table_size));
+
         table_size
     }
 }
@@ -158,8 +128,6 @@ fn form_speffect_map(param_goods_ptr: *const u64, param_goods_list_size: u16) ->
             goods_map.insert(item_id, item_value);
         }
     }
-
-    log("Goods table formed");
 
     goods_map
 }
@@ -210,7 +178,6 @@ pub unsafe extern "C" fn DllMain(_hmodule: u64, reason: u32) -> bool {
                         let starlight_shard_speffect_param: &mut SP_EFFECT_PARAM_ST = &mut *(starlight_shard_ptr as *mut SP_EFFECT_PARAM_ST);
 
                         starlight_shard_speffect_param.set_effect_endurance(-1.0);
-                        log(&format!("Starlight Shard effect endurance changed to {}", starlight_shard_speffect_param.effect_endurance()));
 
                         is_starlight_shard_modded = true;
                     }
@@ -218,24 +185,18 @@ pub unsafe extern "C" fn DllMain(_hmodule: u64, reason: u32) -> bool {
 
                 // Set event flag for persistent state save
                 if !is_starlight_shards_event_flag_active && is_player_have_starlight_shard_speffect {
-                    unsafe {
-                        event_flags_manager.virtual_memory_flag.flag_blocks.as_mut().unwrap().set(EventFlag::from(STARLIGHT_SHARD_SPEFFECT_EVENT_FLAG), true);
-                        let x = event_flags_manager.virtual_memory_flag.flag_blocks.as_mut().unwrap().get(EventFlag::from(STARLIGHT_SHARD_SPEFFECT_EVENT_FLAG));
-                        log(&format!("Starlight shards event flag set to {}", x));
-                    }
+                    unsafe { event_flags_manager.virtual_memory_flag.flag_blocks.as_mut().unwrap().set(EventFlag::from(STARLIGHT_SHARD_SPEFFECT_EVENT_FLAG), true) };
                 }
 
                 // Apply Starlight Shards effect on game load if it was applied in previous session
                 if is_starlight_shards_event_flag_active && is_player_alive && !is_player_have_starlight_shard_speffect {
                     main_player.chr_ins.apply_speffect(STARLIGHT_SHARD_SPEFFECT_ID, true);
-                    log("Starlight Shard speffect applied based on the event flag");
                 }
 
                 // Remove on death
                 if !is_player_alive && is_player_have_starlight_shard_speffect {
                     main_player.chr_ins.remove_speffect(STARLIGHT_SHARD_SPEFFECT_ID);
                     unsafe { event_flags_manager.virtual_memory_flag.flag_blocks.as_mut().unwrap().set(EventFlag::from(STARLIGHT_SHARD_SPEFFECT_EVENT_FLAG), false) };
-                    log("Player died, removing Starlight Shard effect and setting event flag to false");
                     return;
                 }
             },
